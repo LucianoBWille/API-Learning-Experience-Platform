1b2d6086e8bd3b715b901a0584a7785e306a2b77
LucianoBWille <00lucianobwille00@gmail.com>
2022-07-28 18:48:03 -0300

    CI tweaks for deploying on heroku

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('apiDoc', app, document);
 
-  await app.listen(3000);
+  // await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
